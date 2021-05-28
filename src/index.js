f9cd1f45ff60b1a18e967056fccdaad5384dbb69
Vinay Raikar <rockraikar@gmail.com>
2021-05-28 09:01:30 +0530

    Add swiper

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-console.log("hello");
-setTimeout(() => {
-  document.body.style.background = "aliceblue";
-}, 3000);
+import Swiper from "swiper";
+import SwiperCore, { Keyboard, Mousewheel } from "swiper/core";
+import "swiper/swiper-bundle.css";
+
+SwiperCore.use([Keyboard, Mousewheel]);
+
+function init() {
+  const swiper = new Swiper(".swiper-container", {
+    direction: "vertical",
+    loop: true,
+    keyboard: { enabled: true, onlyInViewport: true, pageUpDown: true },
+    mousewheel: {
+      invert: true,
+    },
+  });
+}
+
+init();
